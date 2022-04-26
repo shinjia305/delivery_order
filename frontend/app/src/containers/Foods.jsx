169ec517c2301cb8a87244811686effde280651f
Asai Shinji <33193927+shinjia305@users.noreply.github.com>
2022-04-26 22:10:55 +0900

    商品一覧のAPIを実行

--- frontend/app/src/containers/Foods.jsx
+++ frontend/app/src/containers/Foods.jsx
@@ -1,14 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { fetchFoods } from '../apis/foods';
 
-export const Foods = ({
-  match
-}) => {
+export const Foods = () => {
+  useEffect(() => {
+    fetchFoods(1)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       フード一覧
-      <p>
-      restaurantsIdは {match.params.restaurantsId} です
-      </p>
     </Fragment>
   )
 }
